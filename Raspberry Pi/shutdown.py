68d05cc2b8895e75d23eaf766938bb584f078d55
D-an-W <dan.whitehead@icloud.com>
2024-01-27 10:34:13 +0000

    Update shutdown.py

--- Raspberry Pi/shutdown.py
+++ Raspberry Pi/shutdown.py
@@ -1,14 +1,4 @@
-import gpiozero as GPIO
-from time import sleep
-from os import system
-
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-while True:
-        buttonState = GPIO.input(5)
-        if buttonState == True:
-                print("Switch was set to off!")
-                system("shutdown now -h")
-                sleep(1)
+from gpiozero import Button
+import os
+Button(3).wait_for_release()
+os.system("sudo shutdown now -h")
